cb798ff52a337655b343c02a68c165ed130f0dd8
1lynnj <lynn@jansheski.com>
2022-04-26 10:20:08 -0700

    final first draft

--- pomodoro-wip/main.py
+++ pomodoro-wip/main.py
@@ -11,35 +11,43 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 check_marks = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
-
+def reset_timer():
+    window.after_cancel(timer)
+    global reps
+    global check_marks
+    check_marks = 0
+    reps = 0
+    canvas.itemconfig(timer_text, text='00:00')
+    timer_label.config(text='Timer', fg=GREEN)
+    checks.config(text='✓' * check_marks)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
     global reps
     global check_marks
+    reps += 1
     work_sec = WORK_MIN * 60
     short_break_sec = SHORT_BREAK_MIN * 60
     long_break_sec = LONG_BREAK_MIN * 60
 
-    while reps < 9:
-        reps += 1
-        print(reps)
-        print(check_marks)
-        if reps % 2 != 0:
-            count_down(work_sec)
-            check_marks += 1
-            checks.config(text='✓' * check_marks)
+    if reps % 8 == 0:
+        timer_label.config(text='Break', fg=RED)
+        count_down(long_break_sec)
+        checks.config(text='✓' * check_marks)
 
-        elif reps % 2 == 0:
-            count_down(short_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text='Break', fg=PINK)
+        count_down(5)
+        checks.config(text='✓' * check_marks)
 
-        elif reps == 8:
-            count_down(long_break_sec)
-
-
+    elif reps % 2 != 0:
+        timer_label.config(text='Work', fg=GREEN)
+        count_down(5)
+        check_marks += 1
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -50,8 +58,13 @@ def count_down(count):
         count_sec = f'0{count_sec}'
     canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
-        window.after(1000, count_down, count-1)  #window.after(time in milliseconds, function, change in time)
+        global timer
+        timer = window.after(1000, count_down, count-1)  #window.after(time in milliseconds, function, change in time)
                                                 #1000milliseconds equals 1 second
+    else:
+        start_timer()
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -66,7 +79,7 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
-timer_label = Label(text='Timer', font=(FONT_NAME, 55, 'normal'), bg=YELLOW, fg=GREEN)
+timer_label = Label(text='Timer', font=(FONT_NAME, 55, 'normal'), bg=YELLOW, fg=GREEN, width=5)
 timer_label.grid(column=1, row=0)
 
 start = Button(text='Start', highlightthickness=0, command=start_timer)
@@ -75,7 +88,7 @@ start.grid(column=0, row=2)
 checks = Label(text='', bg=YELLOW, fg=GREEN, font=(FONT_NAME, 30, 'bold'))
 checks.grid(column=1, row=3)
 
-reset = Button(text='Reset', highlightthickness=0)
+reset = Button(text='Reset', highlightthickness=0, command=reset_timer)
 reset.grid(column=2, row=2)
 
 
